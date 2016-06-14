f8503e8e016a0da8a2a0224a23938038dcc7ab5a
Sam Harris <samh@jackrabbitmobile.com>
2016-06-14 12:14:17 -0400

    minor ui changes and variable name changes

--- app/src/main/java/com/samiamharris/mralarmservice/MrActivity.java
+++ app/src/main/java/com/samiamharris/mralarmservice/MrActivity.java
@@ -19,20 +19,20 @@ public class MrActivity extends AppCompatActivity {
         Toast.makeText(this, MrSharedPref.getMostRecentAlarmAsString(), Toast.LENGTH_LONG).show();
     }
 
-    // Setup a recurring alarm every 5 minutes
+    // Setup a recurring alarm every minute
     private void scheduleAlarm() {
         // Construct an intent that will execute the AlarmReceiver
         Intent intent = new Intent(getApplicationContext(), MrAlarmReceiver.class);
         // Create a PendingIntent to be triggered when the alarm goes off
-        final PendingIntent pIntent = PendingIntent.getBroadcast(this, MrAlarmReceiver.REQUEST_CODE,
+        final PendingIntent alarmReceiverIntent = PendingIntent.getBroadcast(this, MrAlarmReceiver.REQUEST_CODE,
                 intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        // Setup periodic alarm every 5 seconds
-        long firstMillis = System.currentTimeMillis(); // alarm is set right away
+        // Setup periodic alarm
+        long rightMeow = System.currentTimeMillis(); // alarm is set right away
         AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
         // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
-                MrService.ALARM_RECURRING_TIME, pIntent);
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, rightMeow,
+                MrService.ALARM_RECURRING_TIME, alarmReceiverIntent);
     }
 }
 
